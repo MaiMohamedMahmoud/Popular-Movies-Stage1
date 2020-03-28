2ebbdb4a2a615f7fd2f173bf010d26ab6a2ce444
MaiMohamedMahmoud <mai.mohamed.mahmoud91@gmail.com>
2020-03-28 16:13:05 +0100

    Api key

--- app/src/main/java/com/marscode/pwn/aflamk/Data/ApiUtils.java
+++ app/src/main/java/com/marscode/pwn/aflamk/Data/ApiUtils.java
@@ -3,7 +3,7 @@ package com.marscode.pwn.aflamk.Data;
 
 public class ApiUtils {
     public static final String BASE_URL = "http://api.themoviedb.org/3/";
-    public final static String API_KEY ="" /** ENTER YOUR API KEY**/;
+    public final static String API_KEY ="9684e8c38d4fdd3e869d83a6df191baa" /** ENTER YOUR API KEY**/;
     public final static String Base_image_URl = "http://image.tmdb.org/t/p/";
     public final static String image_size = "w500";
 
